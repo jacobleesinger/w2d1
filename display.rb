f2cb6cff099fc1572d63280c521bf99f55a12919
App Academy Student <student@appacademy.io>
2015-11-02 15:11:42 -0800

    board render working.

--- display.rb
+++ display.rb
@@ -1,11 +1,35 @@
 require 'colorize'
+require_relative 'cursorable.rb'
+require_relative 'board.rb'
+require_relative 'piece.rb'
 
 class Display
+  include Cursorable
 
-  attr_accessor :cursor
+  attr_accessor :cursor, :board
 
-  def initialize
+  def initialize(board = Board.new)
+    @board = board
     @cursor = [0, 0]
+  end
+
+  def render
+    @board.grid.each_with_index do |row, x|
+      row.each_with_index do |tile, y|
+        if [x,y] == @cursor
+          print tile_type(tile).colorize(:yellow)
+        else
+          print tile_type(tile)
+        end
+      end
+      puts
+    end
+    nil
+  end
+
+  def tile_type(tile)
+    return tile.token if tile.class == Piece
+    "[ ]"
   end
 
 end
--- piece.rb
+++ piece.rb
@@ -1,2 +1,6 @@
 class Piece
+  attr_accessor :token
+  def initialize
+    @token = "[X]"
+  end
 end
