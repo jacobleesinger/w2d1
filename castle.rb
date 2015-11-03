25d62cca2bd0aede209473b49d3cf5cae541350f
App Academy Student <student@appacademy.io>
2015-11-02 18:00:11 -0800

    slight update to castle

--- castle.rb
+++ castle.rb
@@ -4,8 +4,22 @@ class Castle < SlidingPiece
   end
 
   def possible_moves
+    x, y = @position
     moves = []
-    
+    #row # [nil, nil, p, nil, self, nil, P, nil]
+    i = 0
+    until row[i] == self
+      moves << [i, y]
+      if row[i].team == self.team
+        moves = []
+      elsif row[i].team != self.team
+
+        # subarray of row
+        # cannot include friendly pieces
+        # must include self but cannot move to self
+        # can include enemy pieces but only at first or last
+        # can probably move to SlidingPiece
+
   end
 
   def row
